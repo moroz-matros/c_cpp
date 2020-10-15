b04fa23935f98623c417441493cd5d4668309509
moroz <59224300+moroz-matros@users.noreply.github.com>
2020-10-15 04:07:51 +0300

    Revert "Iz1"

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/moroz/Desktop/tp/c_cpp
-# Build directory: /home/moroz/Desktop/tp/c_cpp/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest-build")
-subdirs("test")
